c381bae3c93c2a375019cf22e3d8a6487374d30c
paolo.bertin <paolo.bertin.62@gmail.com>
2020-06-16 10:08:47 +0200

    aggiunto controller WarehouseController

---
+++ src/main/java/it/opensource/ecompany/web/controller/WarehouseController.java
@@ -0,0 +1,12 @@
+package it.opensource.ecompany.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequestMapping("/warehouse")
+@Controller
+public class WarehouseController {
+
+}
