e571361c9bccabeb338022e834d9503a603a31a7
paolo.bertin <paolo.bertin.62@gmail.com>
2020-06-18 19:32:10 +0200

    aggiunti test classe CartBean

--- src/test/java/it/opensource/ecompany/bean/CartBeanTest.java
+++ src/test/java/it/opensource/ecompany/bean/CartBeanTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.number.IsCloseTo.closeTo;
 
 class CartBeanTest {
@@ -22,6 +24,21 @@ class CartBeanTest {
     }
 
     @Test
+    void addedTheSameProducts() {
+
+        Category category = new Category(1L, "Books");
+        Product product = new Product(1L, "Da Visual Basic a Java", "8883780450", category, 29.90F);
+        CartBean cartBean = new CartBean();
+        cartBean.addProductToCart(product);
+        cartBean.addProductToCart(product);
+
+        Double expected = 59.8; // subTotal
+        Double actual = cartBean.getSubTotal();
+
+        assertThat(expected, closeTo(actual, 0.001));
+    }
+
+    @Test
     void addProductToCart() {
 
         Category category = new Category(1L, "Books");
@@ -30,10 +47,9 @@ class CartBeanTest {
         cartBean.addProductToCart(product);
         product = new Product(3L, "Java Web Services", "1449365116", category, 39.90F);
         cartBean.addProductToCart(product);
-        cartBean.setExpressDelivery(true);
 
-        Double expected = 74.8; // totalCost
-        Double actual = cartBean.getTotalCost();
+        Double expected = 69.8; // subTotal
+        Double actual = cartBean.getSubTotal();
 
         assertThat(expected, closeTo(actual, 0.001));
     }
@@ -41,20 +57,84 @@ class CartBeanTest {
     @Test
     void deleteProductToCart() {
 
+        Category category = new Category(1L, "Books");
+        Product product1 = new Product(1L, "Da Visual Basic a Java", "8883780450", category, 29.90F);
+        CartBean cartBean = new CartBean();
+        cartBean.addProductToCart(product1);
+        Product product2 = new Product(3L, "Java Web Services", "1449365116", category, 39.90F);
+        cartBean.addProductToCart(product2);
+
+        cartBean.deleteProductToCart(product1);
+
+        Double expected = 39.9; // subTotal
+        Double actual = cartBean.getSubTotal();
+
+        assertThat(expected, closeTo(actual, 0.001));
     }
 
     @Test
     void getNumberProducts() {
+
+        Category category = new Category(1L, "Books");
+        Product product1 = new Product(1L, "Da Visual Basic a Java", "8883780450", category, 29.90F);
+        CartBean cartBean = new CartBean();
+        cartBean.addProductToCart(product1);
+        Product product2 = new Product(3L, "Java Web Services", "1449365116", category, 39.90F);
+        cartBean.addProductToCart(product2);
+
+        int expected = 2;
+        int actual = cartBean.getNumberProducts();
+
+        assertThat(expected, is(equalTo(actual)));
 
     }
 
     @Test
     public void getShippingCosts() {
 
+        Category category = new Category(1L, "Books");
+        Product product = new Product(1L, "Da Visual Basic a Java", "8883780450", category, 29.90F);
+        CartBean cartBean = new CartBean();
+        cartBean.addProductToCart(product);
+        product = new Product(3L, "Java Web Services", "1449365116", category, 39.90F);
+        cartBean.addProductToCart(product);
+        cartBean.setExpressDelivery(false);
+
+        Double expected = 69.8; // totalCost
+        Double actual = cartBean.getTotalCost();
+
+        assertThat(expected, closeTo(actual, 0.001));
+
     }
 
     @Test
     public void getSubTotal() {
+
+        Category category = new Category(1L, "Books");
+        Product product = new Product(1L, "La matematica degli dèi e gli algoritmi degli uomini", "8883780450", category, 7.99F);
+        CartBean cartBean = new CartBean();
+        cartBean.addProductToCart(product);
+
+        Double expected = 10.99; // totalCost
+        Double actual = cartBean.getTotalCost();
+
+        assertThat(actual, closeTo(expected, 0.001));
+
+    }
+
+    @Test
+    public void getSubTotalWithExpressDelivery() {
+
+        Category category = new Category(1L, "Books");
+        Product product = new Product(1L, "La matematica degli dèi e gli algoritmi degli uomini", "8883780450", category, 7.99F);
+        CartBean cartBean = new CartBean();
+        cartBean.addProductToCart(product);
+        cartBean.setExpressDelivery(true);
+
+        Double expected = 12.99; // totalCost
+        Double actual = cartBean.getTotalCost();
+
+        assertThat(actual, closeTo(expected, 0.001));
 
     }
 
