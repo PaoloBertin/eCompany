8eb7188ba9d981042e22b87237bbe1df3d88b755
paolo.bertin <paolo.bertin.62@gmail.com>
2020-07-23 17:13:47 +0200

    Modificato file insermento nuova categoria

--- src/main/java/it/opensource/ecompany/web/controller/CatalogsController.java
+++ src/main/java/it/opensource/ecompany/web/controller/CatalogsController.java
@@ -1,16 +1,25 @@
 package it.opensource.ecompany.web.controller;
 
-import java.util.List;
-
+import it.opensource.ecompany.bean.CartBean;
+import it.opensource.ecompany.domain.Category;
+import it.opensource.ecompany.service.CategoriesService;
+import it.opensource.ecompany.web.controller.util.Message;
+import it.opensource.ecompany.web.controller.util.UrlUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import it.opensource.ecompany.domain.Category;
-import it.opensource.ecompany.service.CategoriesService;
-import lombok.extern.slf4j.Slf4j;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Locale;
 
 @Profile("html")
 @Slf4j
@@ -20,12 +29,18 @@ public class CatalogsController {
     @Autowired
     private CategoriesService categoriesService;
 
+    @Autowired
+    private CartBean cartBean;
+
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("/catalog")
     public String getAllCategories(Model uiModel) {
 
         List<Category> categories = categoriesService.getAll();
         uiModel.addAttribute("categories", categories);
-        
+
         log.debug("visualizza tutte le categorie");
 
         return "catalog/list";
@@ -41,4 +56,60 @@ public class CatalogsController {
 
         return "catalog/categoriesListAdmin";
     }
+
+    /**
+     * Gestisce la richiesta di creare una nuova categoria
+     *
+     * @return nome vista
+     */
+    @GetMapping(path = "/admin/catalog", params = "form")
+    public String createCategoryForm(Model uiModel) {
+
+        Category category = new Category();
+
+        //        Customer customer = userContext.getCurrentCustomer();
+
+        //        uiModel.addAttribute("customer", customer);
+        //        uiModel.addAttribute("searchForm", new SearchForm());
+        uiModel.addAttribute("cartBean", cartBean);
+        uiModel.addAttribute("category", category);
+
+        return "/catalog/editCategory";
+    }
+
+    /**
+     * Rende persistente i campi del form ricevuto
+     *
+     * @param category
+     * @param bindingResult
+     * @param uiModel
+     * @param httpServletRequest
+     * @param redirectAttributes
+     * @param locale
+     * @return nome vista
+     */
+    @PostMapping(path = "/admin/catalog", params = "form")
+    public String create(@Valid Category category, BindingResult bindingResult, RedirectAttributes redirectAttributes,
+                         Model uiModel, HttpServletRequest httpServletRequest, Locale locale) {
+
+        log.info("Creating category");
+        if (bindingResult.hasErrors()) {
+            uiModel.addAttribute("message", new Message("error",
+                                                        messageSource.getMessage("category_save_fail", new Object[]{},
+                                                                                 locale)));
+            uiModel.addAttribute("category", category);
+            return "catalog/editCategory";
+        }
+        uiModel.asMap().clear();
+        redirectAttributes.addFlashAttribute("message", new Message("success",
+                                                                    messageSource.getMessage("category_save_success",
+                                                                                             new Object[]{}, locale)));
+
+        Category result = categoriesService.saveCategory(category);
+        log.info("category id: " + result.getCategoryid());
+
+        return "redirect:/admin/catalog/" + UrlUtil.encodeUrlPathSegment(category.getCategoryid().toString(),
+                                                                    httpServletRequest);
+    }
+
 }
