5f039fa993c8b3511ebf7929abd75ec05234e48c
paolo.bertin <paolo.bertin.62@gmail.com>
2020-07-28 08:25:29 +0200

    Introdotta struttura codice per i fornitori

---
+++ src/main/java/it/opensource/ecompany/service/impl/SuppliersServiceImpl.java
@@ -0,0 +1,7 @@
+package it.opensource.ecompany.service.impl;
+
+import it.opensource.ecompany.service.SuppliersService;
+
+public class SuppliersServiceImpl implements SuppliersService {
+
+}
---
+++ src/main/java/it/opensource/ecompany/web/controller/SuppliersController.java
@@ -0,0 +1,5 @@
+package it.opensource.ecompany.web.controller;
+
+public class SuppliersController {
+
+}
