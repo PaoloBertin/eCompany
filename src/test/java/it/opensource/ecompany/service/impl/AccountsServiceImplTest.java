e2b829988692af44f3ad171da59bc2b5b49805b1
paolo.bertin <paolo.bertin.62@gmail.com>
2020-08-20 14:25:38 +0200

    Aggiunti test su classe AccountsServiceImpl

---
+++ src/test/java/it/opensource/ecompany/service/impl/AccountsServiceImplTest.java
@@ -0,0 +1,28 @@
+package it.opensource.ecompany.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountsServiceImplTest {
+
+    @Test
+    void getAccountById() {
+
+    }
+
+    @Test
+    void getAllAccounts() {
+
+    }
+
+    @Test
+    void deposit() {
+
+    }
+
+    @Test
+    void withdrawal() {
+
+    }
+}
