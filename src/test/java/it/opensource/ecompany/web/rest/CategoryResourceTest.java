924cff7222a20556ab257aeb9327c56af44f7bc3
paolo.bertin <paolo.bertin.62@gmail.com>
2021-04-29 10:53:16 +0200

    Corretto test eliminazione categoria(a cui ancora non sono associati prodotti).

--- src/test/java/it/opensource/ecompany/web/rest/CategoryResourceTest.java
+++ src/test/java/it/opensource/ecompany/web/rest/CategoryResourceTest.java
@@ -3,7 +3,7 @@ package it.opensource.ecompany.web.rest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import it.opensource.ecompany.domain.Category;
-import org.junit.jupiter.api.Disabled;
+import it.opensource.ecompany.service.CategoriesService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,6 +21,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @SpringBootTest
 class CategoryResourceTest {
+
+    @Autowired
+    CategoriesService categoriesService;
 
     @EnabledIf(expression = "#{environment.acceptsProfiles('h2')}", loadContext = true)
     @Sql({"/db/schema-h2.sql", "/db/data-h2.sql"})
@@ -50,7 +53,6 @@ class CategoryResourceTest {
     public void createCategoryTest(@Autowired MockMvc mvc) throws Exception {
 
         Category category = new Category("eBook");
-
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
         String requestJson = objectMapper.writeValueAsString(category);
@@ -67,13 +69,34 @@ class CategoryResourceTest {
     public void deleteCategoryTest(@Autowired MockMvc mvc) throws Exception {
 
         Category category = new Category(7L, "Arredamento");
-
         ObjectMapper objectMapper = new ObjectMapper();
+        // objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
         String requestJson = objectMapper.writeValueAsString(category);
 
-        mvc.perform(delete("/categories").contentType(MediaType.APPLICATION_JSON)
-                                         .content(requestJson))
+        // aggiunge categoria al db e verifica l'aggiunta
+        mvc.perform(post("/api/categories").contentType(MediaType.APPLICATION_JSON)
+                                           .content(requestJson))
+           .andExpect(jsonPath("$.id", equalTo(7)))
            .andExpect(status().isOk());
-    }
 
+        // verifica che il numero di categoria sia accresciuto di una unita'
+        mvc.perform(get("/api/categories").contentType(MediaType.APPLICATION_JSON))
+           .andExpect(jsonPath("$.length()", equalTo(7)))
+           .andExpect(status().isOk());
+
+        category = categoriesService.getCategoryByName("Arredamento");
+        objectMapper = new ObjectMapper();
+        requestJson = objectMapper.writeValueAsString(category);
+
+        // elimina categoria dal db
+        mvc.perform(delete("/api/categories").contentType(MediaType.APPLICATION_JSON)
+                                             .content(requestJson))
+           .andExpect(status().isOk());
+
+        // verifica che il numero di categoria sia accresciuto di una unita'
+        mvc.perform(get("/api/categories").contentType(MediaType.APPLICATION_JSON))
+           .andExpect(jsonPath("$.length()", equalTo(6)))
+           .andExpect(status().isOk());
+
+    }
 }
